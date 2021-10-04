96b024fcafa669c0a5941ed4e5a407d045de26d7
Jai Chawla <jaichawla2012@gmail.com>
2021-10-04 15:57:32 -0400

    Initial Commit

---
+++ ExerciseTracker/ExerciseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseTrackerApp.swift
+//  ExerciseTracker
+//
+//  Created by Jai Chawla on 10/4/21.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
